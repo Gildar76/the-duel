e09e76ffeeb5a8edee35d93f5f65d3eb3f77869c
Fredrik Andersson <gildar76@gmail.com>
2016-12-31 13:36:01 +0100

    Step 1: Project setup

---
+++ The-duel/The-duel/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace The_duel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}

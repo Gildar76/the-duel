95131c1dd97b7e6ccf4dd1460c31d8f5425f089b
Fredrik Andersson <gildar76@gmail.com>
2017-01-01 18:18:40 +0100

    Step 9: Let’s do some fighting

--- The-duel/The-duel/Program.cs
+++ The-duel/The-duel/Program.cs
@@ -15,21 +15,92 @@ namespace The_duel
             string playerName = Console.ReadLine();
 
             string[] opponentNames = new string[] { "Gildar", "Styrbjorn", "Twizz", "Kharog" };
-            //Weapon test
-            Weapon playerWeapon = new Weapon("test", 1, 5, 50);
-            Character playerCharacter = new Character("Gildar", 20, playerWeapon, 35);
-            Console.WriteLine(playerCharacter.attack());
-            Console.WriteLine(playerCharacter.defend());
-            //playerCharacter.attack();
-            //playerCharacter.defend();
-            Console.WriteLine(playerCharacter.characterName);
 
+            //Weapons. We could create these when we create the character, but creating them as separate variables will be easier to read.
+            Weapon playerWeapon = new Weapon("sword", 1, 3, 25);
+            Weapon opponentWeapon = new Weapon("axe", 1, 4, 5);
 
-            //Console.WriteLine(playerWeapon.swing());
-            //Console.WriteLine(playerWeapon.parry());
+            //Characters
+            Character playerCharacter = new Character(playerName, 20, playerWeapon, 50);
+            Character opponentCharacter = new Character("opponent", 25, opponentWeapon, 40);
 
-            //Adding a Console.readline to prevent window from closing
+            //generate the fight
+            bool fightIsRunning = false;
+            Console.WriteLine("Press enter to start the fight!");
             Console.ReadLine();
+            fightIsRunning = true;
+            while (fightIsRunning)
+            {
+                fight(playerCharacter, opponentCharacter);
+                if (playerCharacter.hitPoints <= 0 || opponentCharacter.hitPoints <= 0)
+                {
+                    fightIsRunning = false;
+                }
+                Console.WriteLine(playerCharacter.characterName + " has " + playerCharacter.hitPoints + " hit points left");
+                Console.WriteLine(opponentCharacter.characterName + " has " + opponentCharacter.hitPoints + " hit points left");
+                Console.ReadLine();
+            }
+            //Fight is done. In the exercise. Step 17 and 18 is inside the while loop, but I decided to put them here instead to make things clearer.
+            if (playerCharacter.hitPoints <= 0)
+            {
+                Console.WriteLine("Player lost!");
+            }
+            if (opponentCharacter.hitPoints <= 0)
+            {
+                Console.WriteLine("Opponent lost!");
+            }
+
+            Console.ReadLine();
+        }
+
+        public static void fight(Character playerCharacter, Character opponentCharacter)
+        {
+            int damageDealt = 0;
+            //Player character round.
+            damageDealt = playerCharacter.attack();
+            if (damageDealt > 0)
+            {
+                if (opponentCharacter.defend())
+                {
+                    Console.WriteLine(playerCharacter.characterName + " Tries to hit " + opponentCharacter.characterName + " with his " + playerCharacter.weapon.weaponName
+                        + ", but " + opponentCharacter.characterName + " parries");
+                } else
+                {
+                    Console.WriteLine(playerCharacter.characterName + " hits " + opponentCharacter.characterName + " with his " + playerCharacter.weapon.weaponName
+                        + " for " + damageDealt + " points of damage" );
+                    opponentCharacter.hitPoints -= damageDealt;
+                }
+            } else
+            {
+                Console.WriteLine(playerCharacter.characterName + " tries to hit " + opponentCharacter.characterName
+                    + " with his " + playerCharacter.weapon.weaponName + " but misses");
+            }
+
+            //Opponent character round
+            damageDealt = 0;
+            damageDealt = opponentCharacter.attack();
+            if (damageDealt > 0)
+            {
+                if (playerCharacter.defend())
+                {
+                    Console.WriteLine(opponentCharacter.characterName + " Tries to hit " + playerCharacter.characterName + " with his " + opponentCharacter.weapon.weaponName
+                        + ", but " + playerCharacter.characterName + " parries");
+                }
+                else
+                {
+                    Console.WriteLine(opponentCharacter.characterName + " hits " + playerCharacter.characterName + " with his " + opponentCharacter.weapon.weaponName
+                        + " for " + damageDealt + " points of damage");
+                    playerCharacter.hitPoints -= damageDealt;
+                }
+            }
+            else
+            {
+                Console.WriteLine(opponentCharacter.characterName + " tries to hit " + playerCharacter.characterName
+                    + " with his " + opponentCharacter.weapon.weaponName + " but misses");
+            }
+
+
+
         }
     }
 }
